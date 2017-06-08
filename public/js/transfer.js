84a9c0df39df2c042525571c6eb57c573c614cdc
AndrewNo <andrew86@ukr.net>
2017-06-08 11:30:01 +0300

    Some changes in style

--- public/js/transfer.js
+++ public/js/transfer.js
@@ -31559,6 +31559,10 @@ process.off = noop;
 process.removeListener = noop;
 process.removeAllListeners = noop;
 process.emit = noop;
+process.prependListener = noop;
+process.prependOnceListener = noop;
+
+process.listeners = function (name) { return [] }
 
 process.binding = function (name) {
     throw new Error('process.binding is not supported');
